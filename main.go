6c55734c739cb7f2e54d81306010fe9f44261720
Mosawer Mahboob <masomahboob@proton.me>
2023-10-17 19:37:40 +0430

    Create a server and HandleFunc for the "/" route.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from SnippetBox"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
