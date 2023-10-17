3e1de6b89dd833bc02051c81cccd81d89914b90c
Mosawer Mahboob <masomahboob@proton.me>
2023-10-17 19:48:43 +0430

    Add two new routes: /snippet/view and /snippet/create. The Handlers just print trivial message.

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
